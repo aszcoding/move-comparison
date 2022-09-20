b8faf29a19afa96be16731ae70762b425bf68457
Ashley Serrano-Ziel <92651735+aszcoding@users.noreply.github.com>
2022-09-20 14:20:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com', {
+    const response = await axios.get('https://www.omdbapi.com', {
         params: { //query string parameters we want to pass along with the request; these will be appended to end of above URL
             apikey: '891aad2f',
             i: movie.imdbID
